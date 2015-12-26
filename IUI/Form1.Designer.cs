7bceb3e45bfd96510e27cf550896a18a717b41a8
kdkevindeng <824261723@qq.com>
2015-12-26 17:16:58 +0200

    IUI第一次

---
+++ IUI/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Configuration;
+namespace IUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = ConfigurationManager.AppSettings["title"];
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.showmain);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.显示ToolStripMenuItem.Text = "显示";
+            this.显示ToolStripMenuItem.Click += new System.EventHandler(this.showmain);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.exitmain);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(284, 262);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.ScrollBarsEnabled = Boolean.Parse(ConfigurationManager.AppSettings["scroll"]);
+            this.webBrowser1.Url = new System.Uri(ConfigurationManager.AppSettings["homepage"], System.UriKind.Absolute);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Width = Int32.Parse(ConfigurationManager.AppSettings["width"]);
+            this.Height = Int32.Parse(ConfigurationManager.AppSettings["heigth"]);
+            this.MaximizeBox = Boolean.Parse(ConfigurationManager.AppSettings["enMaxBox"]);
+            this.Name = ConfigurationManager.AppSettings["title"];
+            this.Text = ConfigurationManager.AppSettings["title"];
+
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Location = new Point(
+                Screen.PrimaryScreen.WorkingArea.Width - this.Width, //屏幕工作区域减去窗体宽度
+                 10); //屏幕工作区域减去窗体高度
+            this.ShowInTaskbar = false;
+            this.Deactivate += new System.EventHandler(this.formdeactive);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ IUI/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ClassLibrary1.UIHide class1 = new ClassLibrary1.UIHide(this, new Timer());
+
+        }
+        public void shmain() 
+        {
+            if (this.Top < 0)
+            {
+                this.Location = new Point(this.Location.X, 1);
+            }
+            else if (this.Left <= 0)
+            {
+                //stopAnchor = AnchorStyles.Left;
+            }
+            else if (this.Left >= Screen.PrimaryScreen.Bounds.Width - this.Width)
+            {
+                //stopAnchor = AnchorStyles.Right;
+            }
+            else if (this.Top >= Screen.PrimaryScreen.Bounds.Height - this.Height)
+            {
+                //stopAnchor = AnchorStyles.Bottom;
+            }
+            else
+            {
+                //stopAnchor = AnchorStyles.None;
+            }
+            this.Show();                                //窗体显示
+            this.WindowState = FormWindowState.Normal;  //窗体状态默认大小
+            this.Activate();
+        }
+        private void showmain(object sender, EventArgs e)
+        {
+            shmain();
+        }
+
+        private void exitmain(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showmain(object sender, MouseEventArgs e)
+        {
+            shmain();
+        }
+
+        private void formdeactive(object sender, EventArgs e)
+        {
+            if (this.Top == 1)
+            {
+                this.Location = new Point(this.Location.X, 0);
+            }
+        }
+
+    }
+}
